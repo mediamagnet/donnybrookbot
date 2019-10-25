d98bc3ae78ae98f131767ac1128e671ce484ac7c
MediaMagnet <nathanec@gmail.com>
2019-10-25 16:26:33 -0500

    temp file creation.

--- main.go
+++ main.go
@@ -1,10 +1,60 @@
-package Donnybrook
+package main
 
 import (
+	"fmt"
+	"github.com/bwmarrin/discordgo"
+	"github.com/joho/godotenv"
+	"io/ioutil"
 	"log"
 	"os"
-	"github.com/joho/godotenv"
-	"github.com/bwmarrin/discordgo"
-	)
+	"os/signal"
+	"syscall"
+)
 
-discord, err := disscordgo.New("Bot " + )
+func main () {
+
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	file, err := ioutil.TempFile(".", "game.*.race")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer os.Remove(file.Name())
+
+	fmt.Println(file.Name)
+
+	dstoken := os.Getenv("DISCORD_TOKEN")
+	dg, err := discordgo.New("Bot " + dstoken)
+	if err != nil {
+		log.Fatal("Error creating Discord session,", err)
+		return
+	}
+
+	dg.AddHandler(messageCreate)
+
+	err = dg.Open()
+	if err != nil {
+		log.Fatal("Error opening connection,", err)
+		return
+	}
+
+	fmt.Println("Bot is now running. Press CTRL-C to exit.")
+	sc := make(chan os.Signal, 1)
+	signal.Notify(sc, syscall.SIGINT, syscall.SIGTERM, os.Interrupt, os.Kill)
+	<-sc
+
+	dg.Close()
+}
+
+func messageCreate(s *discordgo.Session, m *discordgo.MessageCreate) {
+	if m.Author.ID == s.State.User.ID {
+		return
+	}
+
+	if m.Content == ".ping" {
+		s.ChannelMessageSend(m.ChannelID, "Pong!")
+	}
+}
