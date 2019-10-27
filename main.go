181646d742dc98f007c42cf25b3e2a412a9c50df
MediaMagnet <nathanec@gmail.com>
2019-10-27 14:33:17 -0500

    Added more commands.

--- main.go
+++ main.go
@@ -3,20 +3,24 @@ package main
 import (
 	"database/sql"
 	"fmt"
+	"github.com/bwmarrin/dgvoice"
 	"github.com/bwmarrin/discordgo"
 	"github.com/joho/godotenv"
 	_ "github.com/lib/pq"
+	"io/ioutil"
 	"log"
+	"math/rand"
 	"os"
 	"os/signal"
 	"syscall"
 	"time"
 )
+
 var db *sql.DB
 var err error
 var atime = time.Now()
 
-func main () {
+func main() {
 
 	err := godotenv.Load()
 	if err != nil {
@@ -44,14 +48,30 @@ func main () {
 		panic(err)
 	}
 
-	fmt.Println("Connected to DB")
-
 	dstoken := os.Getenv("DISCORD_TOKEN")
 	dg, err := discordgo.New("Bot " + dstoken)
 	if err != nil {
 		log.Fatal("Error creating Discord session,", err)
 		return
 	}
+
+	guildid := os.Getenv("DISCORD_GUILD")
+	chanid := os.Getenv("DISCORD_VOICE")
+	dgv, err := dg.ChannelVoiceJoin(guildid, chanid, false, true)
+	if err != nil {
+		log.Fatal(err)
+		return
+	}
+
+	fmt.Println("Playing Audio")
+
+		files, _ := ioutil.ReadDir("./audio")
+		for _, f := range files {
+			fmt.Println("Play Audio File:", f.Name())
+			dg.UpdateStatus(0, f.Name())
+			dgvoice.PlayAudioFile(dgv, fmt.Sprintf("%s%s", "./audio", f.Name()), make(chan bool))
+		}
+	dgv.Close()
 
 	dg.AddHandler(messageCreate)
 
@@ -69,43 +89,64 @@ func main () {
 	dg.Close()
 }
 
+func randomInt(min, max int) int {
+	return min + rand.Intn(max-min)
+}
+
+func randomString(len int) string {
+	bytes := make([]byte, len)
+	for i := 0; i < len; i++ {
+		bytes[i] = byte(randomInt(65, 90))
+	}
+	return string(bytes)
+}
+
 func messageCreate(s *discordgo.Session, m *discordgo.MessageCreate) {
 	if m.Author.ID == s.State.User.ID {
 		return
 	}
 
-
 	// if m.Content == ".ready" {
-		// s.ChannelMessageSend(m.ChannelID, m.Author.Username + " has joined the race.")
-		// var sqlStatement = `
-		// INSERT INTO races (name, starttime)
-		// VALUES ($1, $2)`
-		// _, err = db.Exec(sqlStatement, m.Author.Username, time.Now())
-		// if err != nil {
-		//	panic(err)
-		// }
+	// s.ChannelMessageSend(m.ChannelID, m.Author.Username + " has joined the race.")
+	// var sqlStatement = `
+	// INSERT INTO races (name, starttime)
+	// VALUES ($1, $2)`
+	// _, err = db.Exec(sqlStatement, m.Author.Username, time.Now())
+	// if err != nil {
+	//	panic(err)
+	// }
 	// }
 	switch {
+	// Set up Race
+	case m.Content == ".setup":
+		var raceid = randomString(4)
+		s.ChannelMessageSend(m.ChannelID, m.Author.Username+", ready to start race "+raceid)
+	// Join Race
 	case m.Content == ".join":
-		s.ChannelMessageSend(m.ChannelID, m.Author.Username + " has joined the race.")
+		s.ChannelMessageSend(m.ChannelID, m.Author.Username+" has joined the race.")
+	// Ready up for race
 	case m.Content == ".ready":
-		s.ChannelMessageSend(m.ChannelID, m.Author.Username + " has readied up.")
+		s.ChannelMessageSend(m.ChannelID, m.Author.Username+" has readied up.")
+	// Start Race once all ready
 	case m.Content == ".start":
+		
 		s.ChannelMessageSend(m.ChannelID, "All racers have readied")
-		time.Sleep(1*time.Second)
+		time.Sleep(1 * time.Second)
 		s.ChannelMessageSend(m.ChannelID, "Starting in 3.")
-		time.Sleep(1*time.Second)
+		time.Sleep(1 * time.Second)
 		s.ChannelMessageSend(m.ChannelID, "2.")
-		time.Sleep(1*time.Second)
+		time.Sleep(1 * time.Second)
 		s.ChannelMessageSend(m.ChannelID, "1.")
-		time.Sleep(1*time.Second)
+		time.Sleep(1 * time.Second)
 		s.ChannelMessageSend(m.ChannelID, "Go!")
+	// You finish the Race
 	case m.Content == ".done":
 		var time2 = time.Now()
 		var endtime1 = time2.Sub(atime)
-		var endtime = endtime1.Truncate(1*time.Millisecond)
-		s.ChannelMessageSend(m.ChannelID, m.Author.Username + " has finished the race in: " + endtime.String())
+		var endtime = endtime1.Truncate(1 * time.Millisecond)
+		s.ChannelMessageSend(m.ChannelID, m.Author.Username+" has finished the race in: "+endtime.String())
+	// Quit the race
 	case m.Content == ".forfit":
-		s.ChannelMessageSend(m.ChannelID, m.Author.Username + " has forfit the race, hope you join us for the next race!")
+		s.ChannelMessageSend(m.ChannelID, m.Author.Username+" has forfeit the race, hope you join us for the next race!")
 	}
 }
