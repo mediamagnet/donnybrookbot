4a8f6f8f8ea43891d47aad267894cd3454dec4cf
MediaMagnet <nathanec@gmail.com>
2019-11-01 20:07:21 -0500

    Help text in one embed, and some basic you're here already stupid checking.

--- main.go
+++ main.go
@@ -16,6 +16,7 @@ import (
 	_ "github.com/lib/pq"
 )
 
+var staticuser = "108344508940316672"
 var db *sql.DB
 var err error
 var atime = time.Now()
@@ -26,27 +27,6 @@ func main() {
 	err := godotenv.Load()
 	if err != nil {
 		log.Fatal("Error loading .env file")
-	}
-
-	host := os.Getenv("PG_HOST")
-	port := 5432
-	user := os.Getenv("PG_USER")
-	password := os.Getenv("PG_PASS")
-	dbname := os.Getenv("PG_NAME")
-
-	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s "+
-		"password=%s dbname=%s sslmode=disable",
-		host, port, user, password, dbname)
-
-	db, err := sql.Open("postgres", psqlInfo)
-	if err != nil {
-		panic(err)
-	}
-	defer db.Close()
-
-	err = db.Ping()
-	if err != nil {
-		panic(err)
 	}
 
 	dstoken := os.Getenv("DISCORD_TOKEN")
@@ -72,6 +52,32 @@ func main() {
 	dg.Close()
 }
 
+func initDB() {
+
+	dbhost := os.Getenv("PG_HOST")
+	dbport := 5432
+	dbuser := os.Getenv("PG_USER")
+	dbpass := os.Getenv("PG_PASS")
+	dbname := os.Getenv("PG_NAME")
+
+	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s "+
+		"password=%s dbname=%s sslmode=disable",
+		dbhost, dbport, dbuser, dbpass, dbname)
+
+	db, err := sql.Open("postgres", psqlInfo)
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+
+	err = db.Ping()
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("Connected to database.")
+}
+
 func randomInt(min, max int) int {
 	return min + rand.Intn(max-min)
 }
@@ -85,6 +91,7 @@ func randomString(len int) string {
 }
 
 func messageCreate(s *discordgo.Session, m *discordgo.MessageCreate) {
+
 	if m.Author.ID == s.State.User.ID {
 		return
 	}
@@ -95,21 +102,34 @@ func messageCreate(s *discordgo.Session, m *discordgo.MessageCreate) {
 		msgstr = strings.TrimPrefix(msgstr, ".setup")
 		msgstr = strings.TrimSpace(msgstr)
 		msgary := strings.Split(msgstr, ",")
-		fmt.Printf("%v", msgary)
-		var arg1 = msgary[0]
-		var arg2 = msgary[1]
-		var raceid = randomString(4)
-		racestring := fmt.Sprintf("%s, has started race %s for %s in category %s.", m.Author.Username, raceid, arg1, arg2)
-		s.ChannelMessageSend(m.ChannelID, racestring)
+		if len(msgary) <= 1 {
+			s.ChannelMessageSend(m.ChannelID, "You're missing either the game or the category of the race. run `.setup gamename, category`")
+		} else {
+			var arg1 = msgary[0]
+			var arg2 = msgary[1]
+			var raceid = randomString(4)
+			racestring := fmt.Sprintf("%s, has started race %s for %s in category %s.", "<"+"@"+m.Author.ID+">", raceid, strings.TrimSpace(arg1), strings.TrimSpace(arg2))
+			s.ChannelMessageSend(m.ChannelID, racestring)
+		}
 	}
 	switch {
 	// Join Race
 	case m.Content == ".join":
-		s.ChannelMessageSend(m.ChannelID, m.Author.Username+" has joined the race.")
-
+		if m.Author.ID == staticuser {
+			s.ChannelMessageSend(m.ChannelID, "<"+"@"+m.Author.ID+">"+" You've already joined the race please wait for the race to start.")
+		} else {
+			s.ChannelMessageSend(m.ChannelID, "<"+"@"+m.Author.ID+">"+" has joined the race.")
+		}
 	// Ready up for race
 	case m.Content == ".ready":
-		s.ChannelMessageSend(m.ChannelID, m.Author.Username+" has readied up.")
+		if m.Author.ID == staticuser {
+			s.ChannelMessageSend(m.ChannelID, "<"+"@"+m.Author.ID+">"+" You've already readied up for the race if you need to leave use `.unready`.")
+		} else {
+			s.ChannelMessageSend(m.ChannelID, "<"+"@"+m.Author.ID+">"+" has readied up.")
+		}
+	// unready
+	case m.Content == ".unready":
+		s.ChannelMessageSend(m.ChannelID, "<"+"@"+m.Author.ID+">"+" has unreadied")
 	// Start Race once all ready
 	case m.Content == ".start":
 		s.ChannelMessageSend(m.ChannelID, "All racers have readied")
@@ -126,23 +146,36 @@ func messageCreate(s *discordgo.Session, m *discordgo.MessageCreate) {
 		var time2 = time.Now()
 		var endtime1 = time2.Sub(atime)
 		var endtime = endtime1.Truncate(1 * time.Millisecond)
-		s.ChannelMessageSend(m.ChannelID, m.Author.Username+" has finished the race in: "+endtime.String())
+		s.ChannelMessageSend(m.ChannelID, "<"+"@"+m.Author.ID+">"+" has finished the race in: "+endtime.String())
 	// Quit the race
 	case m.Content == ".forfeit":
-		s.ChannelMessageSend(m.ChannelID, m.Author.Username+" has forfeit the race, hope you join us for the next race!")
+		s.ChannelMessageSend(m.ChannelID, "<"+"@"+m.Author.ID+">"+" has forfeit the race, hope you join us for the next race!")
 	// Help text
 	case m.Content == ".help":
 		s.ChannelMessageSendEmbed(m.ChannelID, &discordgo.MessageEmbed{
-			Title:       "Donnybrook Help:",
-			Description: "Welcome to the Donnybrook Race bot to get started use: `.setup` to create a race. \n Example: ```.setup A Link to the Past, Any%``` \n `.join` - Join the race. \n `.ready` - Ready up for the race. \n `.start` - Start the race once everyone is ready. \n `.done` - You finished the race. \n `.forfeit` - Leave the race early. \n `.help` - You're reading it.",
-			Color:       0x550000,
+			Title: "Donnybrook Help:",
+			Thumbnail: &discordgo.MessageEmbedThumbnail{
+				URL:    "https://cdn.discordapp.com/avatars/637392848307748895/7b5cb5a0cb148a5119a84f8a8201169f.png?size=128",
+				Width:  128,
+				Height: 128,
+			},
+			Description: "Welcome to the Donnybrook Race bot to get started use: `.setup` to create a race. \n" +
+				"Example: ```.setup A Link to the Past, Any%``` \n" +
+				"`.join` - Join the race. \n" +
+				"`.ready` - Ready up for the race. \n " +
+				"`.unready` - Unready from the race. \n" +
+				"`.start` - Start the race once everyone is ready. \n" +
+				"`.done` - You finished the race. \n" +
+				"`.forfeit` - Leave the race early.\n" +
+				"`.help` - You're reading it.",
+			Color: 0x550000,
 			Author: &discordgo.MessageEmbedAuthor{
 				URL:     "https://donnybrookbot.xyz",
 				Name:    "Donnybrook",
 				IconURL: "https://cdn.discordapp.com/avatars/637392848307748895/7b5cb5a0cb148a5119a84f8a8201169f.png?size=128"},
 			Footer: &discordgo.MessageEmbedFooter{
 				Text:    slogan,
-				IconURL: "https://cdn.discordapp.com/embed/avatars/0.png"}})
+				IconURL: "https://cdn.discordapp.com/avatars/637392848307748895/7b5cb5a0cb148a5119a84f8a8201169f.png?size=16"}})
 	}
 
 }
