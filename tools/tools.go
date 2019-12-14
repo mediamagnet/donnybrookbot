313607f565e27decbdeccade0bdef78a04b60387
MediaMagnet <nathanec@gmail.com>
2019-12-13 22:25:39 -0600

    Checks things.

--- tools/tools.go
+++ tools/tools.go
@@ -320,3 +320,14 @@ func PlayAudioFile(v *discordgo.VoiceConnection, filename string) {
 		}
 	}
 }
+
+func ComesFromDM(s *discordgo.Session, m *discordgo.MessageCreate) bool {
+	channel, err := s.State.Channel(m.ChannelID)
+	if err != nil {
+		if channel, err = s.Channel(m.ChannelID); err != nil {
+			return false
+		}
+	}
+
+	return channel.Type == discordgo.ChannelTypeDM
+}
