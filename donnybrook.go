7f6c6d927d88d68d70e0d5e0362432593b7cfb02
MediaMagnet <nathanec@gmail.com>
2020-02-20 04:13:05 +0000

    fixed volume preset.

--- donnybrook.go
+++ donnybrook.go
@@ -72,13 +72,6 @@ func connect(s *discordgo.Session, c *discordgo.Connect) {
 			fmt.Println(err)
 		}
 		time.Sleep(15 * time.Minute)
-		settingsInsert := tools.Settings{
-			GuildID: guildName[1],
-			Volume:  100,
-		}
-		tools.MonSettings("donnybrook", "settings", settingsInsert )
-
-
 	}
 
 }
