eb197b0f6c0e20fe67f3390a7222fa9bbd890302
MediaMagnet <nathanec@gmail.com>
2020-03-09 23:58:55 -0500

    The easter eggs get deeper.

--- botmain/botmain.go
+++ botmain/botmain.go
@@ -17,38 +17,42 @@ func BotMain(s *discordgo.Session, m *discordgo.MessageCreate) {
 	// Help text
 	case m.Content == ".help":
 		_ = s.ChannelMessageDelete(m.ChannelID, m.ID)
-		_, _ = s.ChannelMessageSendEmbed(m.ChannelID, &discordgo.MessageEmbed{
-			Title: "Donnybrook Help:",
-			Author: &discordgo.MessageEmbedAuthor{
-				URL:     "https://donnybrookbot.xyz",
-				Name:    "Donnybrook",
-				IconURL: "https://cdn.discordapp.com/avatars/637392848307748895/b20c5e4c7ac3587a7c2438a78722b425.png?size=128"},
-			Thumbnail: &discordgo.MessageEmbedThumbnail{
-				URL:    "https://cdn.discordapp.com/avatars/637392848307748895/b20c5e4c7ac3587a7c2438a78722b425.png?size=128",
-				Width:  128,
-				Height: 128,
-			},
-			Color:       0x550000,
-			Description: "Welcome to the Donnybrook Race bot, Here's some useful commands: \n",
-			Fields: []*discordgo.MessageEmbedField{
-				{Name: ".setup Game, Category", Value: "Setup a race"},
-				{Name: ".join <race id>", Value: "Join a race with the specified id"},
-				{Name: ".ready", Value: "Ready up for the race after you're done setting up"},
-				{Name: ".unready", Value: "Leave the ready state for the race."},
-				{Name: ".start", Value: "Once everyone is ready start the race."},
-				{Name: ".done", Value: "Once you finish the race use this."},
-				{Name: ".forfeit", Value: "Not able to keep going use this to exit the race."},
-				{Name: ".help", Value: "You're reading it."},
-				{Name: ".invite", Value: "Got a question? join the Donnybrook discord here: https://discord.gg/cyZzPZY"},
-				{Name: ".voicejoin", Value: "Join a voice channel for voiced countdown. Must be in voice channel for this to work."},
-				{Name: ".voiceleave", Value: "Leave the voice channel"},
-				{Name: ".tts", Value: "Say something in your voice chat"},
-				{Name: ".cleanup", Value: "Cleans messages in channel command is run in. User must have `Manage Message` permissions."},
-				{Name: ".scatter <Channels to scatter to>", Value: "Scatter users to the provided channels. User must have `Manage Server` Permissions."},
-				{Name: ".lick", Value: "..."}},
-			Footer: &discordgo.MessageEmbedFooter{
-				Text:    slogan,
-				IconURL: "https://cdn.discordapp.com/avatars/637392848307748895/b20c5e4c7ac3587a7c2438a78722b425.png?size=16"}})
+		if m.Author.ID == "668208024862588928" {
+			s.ChannelMessageSend(m.ChannelID, "]help")
+		} else {
+			_, _ = s.ChannelMessageSendEmbed(m.ChannelID, &discordgo.MessageEmbed{
+				Title: "Donnybrook Help:",
+				Author: &discordgo.MessageEmbedAuthor{
+					URL:     "https://donnybrookbot.xyz",
+					Name:    "Donnybrook",
+					IconURL: "https://cdn.discordapp.com/avatars/637392848307748895/b20c5e4c7ac3587a7c2438a78722b425.png?size=128"},
+				Thumbnail: &discordgo.MessageEmbedThumbnail{
+					URL:    "https://cdn.discordapp.com/avatars/637392848307748895/b20c5e4c7ac3587a7c2438a78722b425.png?size=128",
+					Width:  128,
+					Height: 128,
+				},
+				Color:       0x550000,
+				Description: "Welcome to the Donnybrook Race bot, Here's some useful commands: \n",
+				Fields: []*discordgo.MessageEmbedField{
+					{Name: ".setup Game, Category", Value: "Setup a race"},
+					{Name: ".join <race id>", Value: "Join a race with the specified id"},
+					{Name: ".ready", Value: "Ready up for the race after you're done setting up"},
+					{Name: ".unready", Value: "Leave the ready state for the race."},
+					{Name: ".start", Value: "Once everyone is ready start the race."},
+					{Name: ".done", Value: "Once you finish the race use this."},
+					{Name: ".forfeit", Value: "Not able to keep going use this to exit the race."},
+					{Name: ".help", Value: "You're reading it."},
+					{Name: ".invite", Value: "Got a question? join the Donnybrook discord here: https://discord.gg/cyZzPZY"},
+					{Name: ".voicejoin", Value: "Join a voice channel for voiced countdown. Must be in voice channel for this to work."},
+					{Name: ".voiceleave", Value: "Leave the voice channel"},
+					{Name: ".tts", Value: "Say something in your voice chat"},
+					{Name: ".cleanup", Value: "Cleans messages in channel command is run in. User must have `Manage Message` permissions."},
+					{Name: ".scatter <Channels to scatter to>", Value: "Scatter users to the provided channels. User must have `Manage Server` Permissions."},
+					{Name: ".lick", Value: "..."}},
+				Footer: &discordgo.MessageEmbedFooter{
+					Text:    slogan,
+					IconURL: "https://cdn.discordapp.com/avatars/637392848307748895/b20c5e4c7ac3587a7c2438a78722b425.png?size=16"}})
+		}
 	// Helf text
 	case m.Content == ".helf":
 		_ = s.ChannelMessageDelete(m.ChannelID, m.ID)
